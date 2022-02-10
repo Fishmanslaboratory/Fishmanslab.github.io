753bcbfaf7259ab2711101260ad8f07fd5aa521a
Prathyusha-Guduru <prathyushaguduru28@gmail.com>
2022-02-10 22:04:14 +0530

    Update : about section

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var myVideo = document.getElementById('myVideo');
+// $(function(){
+//     console.log("wgwrbrwb")
+//     let start = 0;
+//     let end = $(".num").html()
+//     let speed = 5;
+//     setInterval(function(){
+//         if(start < end){
+//             start++;
+//         }
+//         $(".num").html(start);
+//     },speed);
+// })
 
-VisSense.VisMon.Builder(VisSense(myVideo))
-.on('fullyvisible', function() {
-    myVideo.play();
-})
-.on('hidden', function() {
-    myVideo.pause();
-})
-.build()
-.start();
 
-
-
-
+element.innerHTML = 12 // Native, or...
+$('.odometer').html(12) // with jQuery
